6ae269eeefe7a162e50d5b7ce4587f1c8c6b3d1a
Keiran Raine <keiranmraine@gmail.com>
2016-03-31 16:04:03 +0100

    Version bump

--- lib/Sanger/CGP/Vagrent.pm
+++ lib/Sanger/CGP/Vagrent.pm
@@ -1,7 +1,7 @@
 package Sanger::CGP::Vagrent;
 
 ##########LICENCE##########
-# Copyright (c) 2014 Genome Research Ltd.
+# Copyright (c) 2014-2016 Genome Research Ltd.
 #
 # Author: Cancer Genome Project cgpit@sanger.ac.uk
 #
@@ -26,7 +26,7 @@ use strict;
 use Const::Fast qw(const);
 
 use base 'Exporter';
-our $VERSION = '2.1.3';
+our $VERSION = '3.0.0';
 our @EXPORT = qw($VERSION);
 
 1;
